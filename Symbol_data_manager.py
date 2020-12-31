ba706f0eb7e4ceb58ec7561221c9d81556894b07
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2020-12-31 14:33:59 -0500

    Update Symbol_data_manager.py

--- Symbol_data_manager.py
+++ Symbol_data_manager.py
@@ -550,6 +550,8 @@ def register(symbol):
 		#p="http://localhost:8080/Register?symbol="+symbol+"&feedtype=TOS&feedtype=L1"
 		p ="http://localhost:8080/GetSnapshot?symbol="+symbol+"&feedtype=L1"
 		r= requests.get(p)
+		p ="http://localhost:8080/GetSnapshot?symbol="+symbol+"&feedtype=TOS"
+		r= requests.get(p)
 
 
 		reg_count+=1
