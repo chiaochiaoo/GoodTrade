1cc3151b0c81390be32fd5e097b326fa287c27fc
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2021-01-03 14:34:01 -0500

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -169,47 +169,167 @@ class alert(pannel):
 					self.alert_pannel.add_alerts([symbol,time,alert_type])
 					self.set_latest_alert(symbol, alert_type, time)
 
+class double_alert(pannel):
+
+	def __init__(self,frame,data:Symbol_data_manager,alert_pannel:all_alerts):
+
+		super().__init__(frame)
+
+		self.alert_pannel=alert_pannel
+		self.data = data
+
+		#init the labels. 
+
+	#any alert will need a threshold. deviation. std. 
+	def add_symbol(self,symbol,format,alert_positions,alerts,data_ready):
+
+		l = self.label_count
+
+		self.tickers_labels[symbol] = []
+		i = symbol
+
+		for j in range(len(format)):
+
+			if j==0:
+				self.tickers_labels[i].append(tk.Label(self.frame ,text=format[j],width=self.width[j]))
+				self.label_default_configure(self.tickers_labels[i][j])
+				self.tickers_labels[i][j].grid(row= l+2, column=j,padx=0)
+			elif j==1:
+				self.tickers_labels[i].append(tk.Label(self.frame ,textvariable=format[j],width=self.width[j]))
+				self.label_default_configure(self.tickers_labels[i][j])
+				self.tickers_labels[i][j].grid(row= l+2, column=j,padx=0)
+				format[j].trace('w', lambda *_, text=format[j],label=self.tickers_labels[i][j]: self.status_change_color(text,label))
+
+			#when it is alert label creation, create a trace set for value position 
+			elif j in alert_positions:
+				self.tickers_labels[i].append(tk.Label(self.frame ,textvariable=format[j],width=self.width[j]))
+				self.label_default_configure(self.tickers_labels[i][j])
+				self.tickers_labels[i][j].grid(row= l+2, column=j,padx=0)
+				#unpack value_position,alert_position,alertvals
+				value_position = alerts[j][0]
+				alert_position = alerts[j][1]
+				alertvals = alerts[j][2]
+				format[value_position].trace('w', lambda *_, eval_string=format[j],label=self.tickers_labels[i][j],alertsvals=alert_vals,ready=data_ready,status=format[1]: self.alert(eval_string,label,alertsvals,ready,status))
+
+			elif j>1:
+				self.tickers_labels[i].append(tk.Label(self.frame ,textvariable=format[j],width=self.width[j]))
+				self.label_default_configure(self.tickers_labels[i][j])
+				self.tickers_labels[i][j].grid(row= l+2, column=j,padx=0)
+
+		#self.ticker_count +=1
+		self.label_count +=1
+
+		self.rebind(self.canvas,self.frame)
+
+	def delete_symbol(self,symbol):
+
+		for i in self.tickers_labels[symbol]:
+			i.destroy()
+
+		self.tickers_labels.pop(symbol,None)
+
+		self.rebind(self.canvas,self.frame)
 
 
-class highlow(alert):
 
-	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
+	def set_latest_alert(self,symbol,alert,time):
 
-		super().__init__(frame,data,alert_panel)
+		self.data.symbol_last_alert[symbol].set(alert)
+		self.data.symbol_last_alert_time[symbol].set(time)
 
-		self.labels = ["Ticker","Status","Cur Range","Cur High","Cur Low","H. Avg","H. Std","H. Range","Evaluation"]
-		self.width = [8,10,7,7,7,7,7,9,15]
-		self.labels_creator(self.frame)
+	#alert vals: cur, mean, std.
+	def alert(self,eval_string,eval_label,alerts_vals,ready,status):
 
-	def add_symbol(self,symbol):
+		#check how many std it is. `
 
-		status = self.data.symbol_status[symbol]
-		cur_range =self.data.symbol_price_range[symbol]
-		cur_high = self.data.symbol_price_high[symbol]
-		cur_low = self.data.symbol_price_low[symbol]
-		hist_avg= self.data.symbol_data_range_val[symbol]
-		hist_std = self.data.symbol_data_range_std[symbol]
-		hist_range= self.data.symbol_data_range_range[symbol]
-		eva= self.data.symbol_data_range_eval[symbol]
-		time = self.data.symbol_update_time[symbol]
+		#attention, only do the calculation when the database is set. 
 
-		data_ready = self.data.data_ready[symbol]
+		if ready.get() == True and status.get() =="Connected":
 
-		value_position = 2
-		alert_position = 8
-		alert_type = "Intraday Range"
+			symbol= alerts_vals[0]
+			time= alerts_vals[1].get()[:5]
+			
+			cur_price= round(alerts_vals[2].get(),3)
+			mean= round(alerts_vals[3].get(),3)
+			std=  round(alerts_vals[4].get(),3)
+			alert_type = alerts_vals[5]
 
-		#cur, mean, std. symbol, time. 
-		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
-		labels = [symbol,status,cur_range,cur_high,cur_low,hist_avg,hist_std,hist_range,eva]
+			ts = timestamp(time)
 
 
-		#any alert will need a threshold. deviation. std. 
+			if std != 0:
+				cur = round((cur_price-mean)/std,3)
+				eval_string.set(str(cur)+" from mean")
+			else:
+				cur = 0
+				eval_string.set("Unable to process std 0")
 
-		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+			#color. 
 
-	#find a way to bound the special checking value to. hmm. every update.
+			if cur <0.5:
+				eval_label["background"]="white"
+
+			elif cur>0.5 and cur<1:
+				#alert_type = "Moderate "+alert_type
+				eval_label["background"]="#97FEA8"
+				# self.alert_pannel.add_alerts([symbol,time,alert_type])
+				# self.set_latest_alert(symbol, alert_type, time)
+
+			elif cur>1 and cur<2:
+				alert_type = "High "+alert_type
+				eval_label["background"]="yellow"
+				if ts>570:
+					self.alert_pannel.add_alerts([symbol,time,alert_type])
+					self.set_latest_alert(symbol, alert_type, time)
+			else:
+				### Send the alert to alert pannel.
+				alert_type = "Very high "+alert_type
+				eval_label["background"]="red"
+				if ts>570:
+					self.alert_pannel.add_alerts([symbol,time,alert_type])
+					self.set_latest_alert(symbol, alert_type, time)
+
+
+
+# class highlow(alert):
+
+# 	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
+
+# 		super().__init__(frame,data,alert_panel)
+
+# 		self.labels = ["Ticker","Status","Cur Range","Cur High","Cur Low","H. Avg","H. Std","H. Range","Evaluation"]
+# 		self.width = [8,10,7,7,7,7,7,9,15]
+# 		self.labels_creator(self.frame)
+
+# 	def add_symbol(self,symbol):
+
+# 		status = self.data.symbol_status[symbol]
+# 		cur_range =self.data.symbol_price_range[symbol]
+# 		cur_high = self.data.symbol_price_high[symbol]
+# 		cur_low = self.data.symbol_price_low[symbol]
+# 		hist_avg= self.data.symbol_data_range_val[symbol]
+# 		hist_std = self.data.symbol_data_range_std[symbol]
+# 		hist_range= self.data.symbol_data_range_range[symbol]
+# 		eva= self.data.symbol_data_range_eval[symbol]
+# 		time = self.data.symbol_update_time[symbol]
+
+# 		data_ready = self.data.data_ready[symbol]
+
+# 		value_position = 2
+# 		alert_position = 8
+# 		alert_type = "Intraday Range"
+
+# 		#cur, mean, std. symbol, time. 
+# 		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
+# 		labels = [symbol,status,cur_range,cur_high,cur_low,hist_avg,hist_std,hist_range,eva]
+
+
+# 		#any alert will need a threshold. deviation. std. 
+
+# 		#self,symbol,format,width,val_position,alert_position,alert_vals
+# 		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+
+# 	#find a way to bound the special checking value to. hmm. every update.
 
 
 
@@ -247,187 +367,192 @@ class openhigh(alert):
 		labels = [symbol,status,cur_range,cur_open,cur_high,hist_avg,hist_std,hist_range,eva]
 
 
+		alert_positions = [alert_position]
+
+		alerts = {}
+		alerts[alert_position] = [value_position,alert_position,alertvals]
 		#any alert will need a threshold. deviation. std. 
 
 		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+		super().add_symbol(symbol, labels,alert_positions,alerts,data_ready)
 
 	#find a way to bound the special checking value to. hmm. every update.
 
-class openlow(alert):
+# class openlow(alert):
 
-	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
+# 	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
 
-		super().__init__(frame,data,alert_panel)
+# 		super().__init__(frame,data,alert_panel)
 
-		self.labels = ["Ticker","Status","Cur Range","Cur Open","Cur Low","H. Avg","H. Std","H. Range","Evaluation"]
-		self.width = [8,10,7,7,7,7,7,9,15]
-		self.labels_creator(self.frame)
+# 		self.labels = ["Ticker","Status","Cur Range","Cur Open","Cur Low","H. Avg","H. Std","H. Range","Evaluation"]
+# 		self.width = [8,10,7,7,7,7,7,9,15]
+# 		self.labels_creator(self.frame)
 
-	def add_symbol(self,symbol):
+# 	def add_symbol(self,symbol):
 
-		status = self.data.symbol_status[symbol]
-		cur_range =self.data.symbol_price_openlow[symbol]
-		cur_open = self.data.symbol_price_open[symbol]
-		cur_low = self.data.symbol_price_low[symbol]
-		hist_avg= self.data.symbol_data_openlow_val[symbol]
-		hist_std = self.data.symbol_data_openlow_std[symbol]
-		hist_range= self.data.symbol_data_openlow_range[symbol]
+# 		status = self.data.symbol_status[symbol]
+# 		cur_range =self.data.symbol_price_openlow[symbol]
+# 		cur_open = self.data.symbol_price_open[symbol]
+# 		cur_low = self.data.symbol_price_low[symbol]
+# 		hist_avg= self.data.symbol_data_openlow_val[symbol]
+# 		hist_std = self.data.symbol_data_openlow_std[symbol]
+# 		hist_range= self.data.symbol_data_openlow_range[symbol]
 
-		eva= self.data.symbol_data_openlow_eval[symbol]
-		time = self.data.symbol_update_time[symbol]
+# 		eva= self.data.symbol_data_openlow_eval[symbol]
+# 		time = self.data.symbol_update_time[symbol]
 
-		data_ready = self.data.data_ready[symbol]
+# 		data_ready = self.data.data_ready[symbol]
 
-		value_position = 2
-		alert_position = 8
-		alert_type = "Intraday Open-Low"
+# 		value_position = 2
+# 		alert_position = 8
+# 		alert_type = "Intraday Open-Low"
 
-		#cur, mean, std. symbol, time. 
-		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
-		labels = [symbol,status,cur_range,cur_open,cur_low,hist_avg,hist_std,hist_range,eva]
+# 		#cur, mean, std. symbol, time. 
+# 		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
+# 		labels = [symbol,status,cur_range,cur_open,cur_low,hist_avg,hist_std,hist_range,eva]
 
 
-		#any alert will need a threshold. deviation. std. 
+# 		#any alert will need a threshold. deviation. std. 
 
-		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+# 		#self,symbol,format,width,val_position,alert_position,alert_vals
+# 		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
 
-	#find a way to bound the special checking value to. hmm. every update.
+# 	#find a way to bound the special checking value to. hmm. every update.
 
 
-class firstfive(alert):
+# class firstfive(alert):
 
-	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
+# 	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
 
-		super().__init__(frame,data,alert_panel)
+# 		super().__init__(frame,data,alert_panel)
 
-		self.labels = ["Ticker","Status","Cur Range","H. Avg","H. Std","H. Range","Cur Vol","H.Vol Avg","H.Vol Std","H.Vol Range","Evaluation:Range","Evaluation:Volume"]
-		self.width = [8,10,7,7,7,7,7,7,7,12,14,15,15]
-		self.labels_creator(self.frame)
+# 		self.labels = ["Ticker","Status","Cur Range","H. Avg","H. Std","H. Range","Cur Vol","H.Vol Avg","H.Vol Std","H.Vol Range","Evaluation:Range","Evaluation:Volume"]
+# 		self.width = [8,10,7,7,7,7,7,7,7,12,14,15,15]
+# 		self.labels_creator(self.frame)
 
-	def add_symbol(self,symbol):
+# 	def add_symbol(self,symbol):
 
-		status = self.data.symbol_status[symbol]
-		#?
-		cur_range =self.data.first_5_min_range[symbol]
-		cur_vol = self.data.first_5_min_volume[symbol]
+# 		status = self.data.symbol_status[symbol]
+# 		#?
+# 		cur_range =self.data.first_5_min_range[symbol]
+# 		cur_vol = self.data.first_5_min_volume[symbol]
 
-		hist_avg= self.data.symbol_data_first5_val[symbol]
-		hist_std = self.data.symbol_data_first5_std[symbol]
-		hist_range= self.data.symbol_data_first5_range[symbol]
+# 		hist_avg= self.data.symbol_data_first5_val[symbol]
+# 		hist_std = self.data.symbol_data_first5_std[symbol]
+# 		hist_range= self.data.symbol_data_first5_range[symbol]
 
-		hist_v_avg= self.data.symbol_data_first5_vol_val[symbol]
-		hist_v_std = self.data.symbol_data_first5_vol_std[symbol]
-		hist_v_range= self.data.symbol_data_first5_vol_range[symbol]
+# 		hist_v_avg= self.data.symbol_data_first5_vol_val[symbol]
+# 		hist_v_std = self.data.symbol_data_first5_vol_std[symbol]
+# 		hist_v_range= self.data.symbol_data_first5_vol_range[symbol]
 
-		eva= self.data.symbol_data_first5_range_eval[symbol]
-		time = self.data.symbol_update_time[symbol]
+# 		eva= self.data.symbol_data_first5_range_eval[symbol]
+# 		time = self.data.symbol_update_time[symbol]
 
-		data_ready = self.data.data_ready[symbol]
+# 		data_ready = self.data.data_ready[symbol]
 
-		value_position = 2
-		alert_position = 10
-		alert_type = "Opening"
+# 		value_position = 2
+# 		alert_position = 10
+# 		alert_type = "Opening"
 
-		#cur, mean, std. symbol, time. 
-		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
-		labels = [symbol,status,cur_range,hist_avg,hist_std,hist_range,cur_vol,hist_v_avg,hist_v_std,hist_v_range,eva]
+# 		#cur, mean, std. symbol, time. 
+# 		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type]
+# 		labels = [symbol,status,cur_range,hist_avg,hist_std,hist_range,cur_vol,hist_v_avg,hist_v_std,hist_v_range,eva]
 
 
-		#any alert will need a threshold. deviation. std. 
 
-		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
 
-	#find a way to bound the special checking value to. hmm. every update.
 
+# 		#any alert will need a threshold. deviation. std. 
 
+# 		#self,symbol,format,width,val_position,alert_position,alert_vals
+# 		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
 
+# 	#find a way to bound the special checking value to. hmm. every update.
 
-class extremrange(alert):
 
-	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
 
-		super().__init__(frame,data,alert_panel)
+# class extremrange(alert):
 
-		self.labels = ["Ticker","Status","Cur Range","H. Avg","H. Std","H. Range","Evaluation"]
-		self.width = [8,10,7,7,7,15,15]
-		self.labels_creator(self.frame)
+# 	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
 
-	def add_symbol(self,symbol):
+# 		super().__init__(frame,data,alert_panel)
 
-		status = self.data.symbol_status[symbol]
-		#?
-		cur_range =self.data.last_5_min_range[symbol]
+# 		self.labels = ["Ticker","Status","Cur Range","H. Avg","H. Std","H. Range","Evaluation"]
+# 		self.width = [8,10,7,7,7,15,15]
+# 		self.labels_creator(self.frame)
 
-		hist_avg= self.data.symbol_data_normal5_val[symbol]
-		hist_std = self.data.symbol_data_normal5_std[symbol]
-		hist_range= self.data.symbol_data_normal5_range[symbol]
+# 	def add_symbol(self,symbol):
 
-		eva= self.data.symbol_data_normal5_range_eval[symbol]
+# 		status = self.data.symbol_status[symbol]
+# 		#?
+# 		cur_range =self.data.last_5_min_range[symbol]
 
-		time = self.data.symbol_update_time[symbol]
+# 		hist_avg= self.data.symbol_data_normal5_val[symbol]
+# 		hist_std = self.data.symbol_data_normal5_std[symbol]
+# 		hist_range= self.data.symbol_data_normal5_range[symbol]
 
-		data_ready = self.data.data_ready[symbol]
+# 		eva= self.data.symbol_data_normal5_range_eval[symbol]
 
-		value_position = 2
-		alert_position = 6
-		alert_type = "Intraday Range"
-		alert_time = 0
-		#cur, mean, std. symbol, time. 
-		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type,alert_time]
-		labels = [symbol,status,cur_range,hist_avg,hist_std,hist_range,eva]
+# 		time = self.data.symbol_update_time[symbol]
 
+# 		data_ready = self.data.data_ready[symbol]
 
-		#any alert will need a threshold. deviation. std. 
+# 		value_position = 2
+# 		alert_position = 6
+# 		alert_type = "Intraday Range"
+# 		alert_time = 0
+# 		#cur, mean, std. symbol, time. 
+# 		alertvals= [symbol,time,cur_range,hist_avg,hist_std,alert_type,alert_time]
+# 		labels = [symbol,status,cur_range,hist_avg,hist_std,hist_range,eva]
 
-		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
 
-	#find a way to bound the special checking value to. hmm. every update.
+# 		#any alert will need a threshold. deviation. std. 
 
+# 		#self,symbol,format,width,val_position,alert_position,alert_vals
+# 		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
 
+# 	#find a way to bound the special checking value to. hmm. every update.
 
-class extremevolume(alert):
 
-	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
+# class extremevolume(alert):
 
-		super().__init__(frame,data,alert_panel)
+# 	def __init__(self,frame,data:Symbol_data_manager,alert_panel:all_alerts):
 
-		self.labels = ["Ticker","Status","Cur Vol(k)","H. Avg(k)","H. Std(k)","H. Range","Evaluation"]
-		self.width = [8,10,15,7,7,15,15]
-		self.labels_creator(self.frame)
+# 		super().__init__(frame,data,alert_panel)
 
-	def add_symbol(self,symbol):
+# 		self.labels = ["Ticker","Status","Cur Vol(k)","H. Avg(k)","H. Std(k)","H. Range","Evaluation"]
+# 		self.width = [8,10,15,7,7,15,15]
+# 		self.labels_creator(self.frame)
 
-		status = self.data.symbol_status[symbol]
-		#?
+# 	def add_symbol(self,symbol):
 
-		cur_vol = self.data.last_5_min_volume[symbol]
+# 		status = self.data.symbol_status[symbol]
+# 		#?
 
-		hist_avg= self.data.symbol_data_normal5_vol_val[symbol]
-		hist_std = self.data.symbol_data_normal5_vol_std[symbol]
-		hist_range= self.data.symbol_data_normal5_vol_range[symbol]
+# 		cur_vol = self.data.last_5_min_volume[symbol]
 
-		eva= self.data.symbol_data_normal5_vol_eval[symbol]
+# 		hist_avg= self.data.symbol_data_normal5_vol_val[symbol]
+# 		hist_std = self.data.symbol_data_normal5_vol_std[symbol]
+# 		hist_range= self.data.symbol_data_normal5_vol_range[symbol]
 
-		time = self.data.symbol_update_time[symbol]
+# 		eva= self.data.symbol_data_normal5_vol_eval[symbol]
 
-		data_ready = self.data.data_ready[symbol]
+# 		time = self.data.symbol_update_time[symbol]
 
-		value_position = 2
-		alert_position = 6
-		alert_type = "Intraday Open-Low"
+# 		data_ready = self.data.data_ready[symbol]
 
-		alert_time = 0
+# 		value_position = 2
+# 		alert_position = 6
+# 		alert_type = "Intraday Open-Low"
 
-		#cur, mean, std. symbol, time. 
-		alertvals= [symbol,time,cur_vol,hist_avg,hist_std,alert_type,alert_time]
-		labels = [symbol,status,cur_vol,hist_avg,hist_std,hist_range,eva]
-		#any alert will need a threshold. deviation. std. 
+# 		alert_time = 0
 
-		#self,symbol,format,width,val_position,alert_position,alert_vals
-		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+# 		#cur, mean, std. symbol, time. 
+# 		alertvals= [symbol,time,cur_vol,hist_avg,hist_std,alert_type,alert_time]
+# 		labels = [symbol,status,cur_vol,hist_avg,hist_std,hist_range,eva]
+# 		#any alert will need a threshold. deviation. std. 
 
-	#find a way to bound the special checking value to. hmm. every update.
+# 		#self,symbol,format,width,val_position,alert_position,alert_vals
+# 		super().add_symbol(symbol, labels,value_position,alert_position,alertvals,data_ready)
+
+# 	#find a way to bound the special checking value to. hmm. every update.
