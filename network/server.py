92984641dc85c9d1c5d89599674894bacd89a949
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2021-01-13 17:01:36 -0300

    Update server.py

--- network/server.py
+++ network/server.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = 'localhost'  # Standard loopback interface address (localhost)
+HOST = '10.29.10.133'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
