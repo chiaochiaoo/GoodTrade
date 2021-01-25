c584594556c00e92af604f33215cbe4d1b3210dc
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-01-25 14:05:30 -0500

    Update server_ppro.py

--- server/server_ppro.py
+++ server/server_ppro.py
@@ -22,7 +22,6 @@ def market_scanner(queue,sync_lock):
 
 	global sync_number
 
-	threadshold = 50
 	a = pd.read_csv('nasdaq.csv', index_col=0)
 	# print(a)
 	# a = a.set_index('Ticker')
@@ -34,16 +33,17 @@ def market_scanner(queue,sync_lock):
 	while True:
 		for i in ticks:
 
-			while sync_number >30:
-				time.sleep(2)
+			#print(i,sync_number)
+			while sync_number >5:
+				time.sleep(4)
 
 			reg = threading.Thread(target=getinfo,args=(i+".NQ",queue), daemon=True)
 			reg.start()
 			count+=1
-
-			if count%10 == 0:
-				with sync_lock:
-					sync_number	+= 10			
+			time.sleep(0.1)
+			
+			with sync_lock:
+				sync_number	+= 1			
 
 
 
@@ -68,21 +68,21 @@ def test_register():
 	except Exception as e:
 		return True
 
-#def register(symbol):
+# def register(symbol):
 
-	try:
-		p="http://localhost:8080/Register?symbol="+symbol+"&feedtype=L1"
-		r= requests.get(p)
-		print(symbol,"registerd ","total:",reg_count)
+# 	try:
+# 		p="http://localhost:8080/Register?symbol="+symbol+"&feedtype=L1"
+# 		r= requests.get(p)
+# 		print(symbol,"registerd ","total:",reg_count)
 
-		if symbol not in lock:
-			lock[symbol] = False
-		else:
-			lock[symbol] = False
+# 		if symbol not in lock:
+# 			lock[symbol] = False
+# 		else:
+# 			lock[symbol] = False
 
-	except Exception as e:
+# 	except Exception as e:
 
-		print("Register issue",e)
+# 		print("Register issue",e)
 
 def timestamp_seconds(s):
 
@@ -98,63 +98,60 @@ def timestamp_seconds(s):
 def getinfo(symbol,pipe):
 
 	global lock
-	global connection_error
+	#global connection_error
 
+	
 	if symbol not in lock:
 		lock[symbol] = False
 
-	if not connection_error:
+	if not lock[symbol]:
+		try:
+			lock[symbol] = True
+			#######################################################################
 
-		if not lock[symbol]:
-			try:
-				#######################################################################
+			p="http://localhost:8080/Register?symbol="+symbol+"&feedtype=L1"
+			r= requests.get(p)
 
-				p="http://localhost:8080/Register?symbol="+symbol+"&feedtype=L1"
-				r= requests.get(p)
+			print(symbol+" started")
+			p="http://localhost:8080/GetLv1?symbol="+symbol
+			r= requests.get(p)
+			print(symbol+" request complete")
+			#print(r.text)
 
-				lock[symbol] = True
-				p="http://localhost:8080/GetLv1?symbol="+symbol
-				r= requests.get(p)
+			time=find_between(r.text, "MarketTime=\"", "\"")[:-4]
+			open_ = float(find_between(r.text, "OpenPrice=\"", "\""))
+			high = float(find_between(r.text, "HighPrice=\"", "\""))
+			low = float(find_between(r.text, "LowPrice=\"", "\""))
+			vol = int(find_between(r.text, "Volume=\"", "\""))
+			prev_close = float(find_between(r.text, "ClosePrice=\"", "\""))
+			price = float(find_between(r.text, "LastPrice=\"", "\""))
 
-				if(r.text =='<Response><Content>No data available symbol</Content></Response>'):
-					print("No symbol found")
-					black_list.append(symbol)
-					pipe.put(["Unfound",symbol])
-				else:
-
-					time=find_between(r.text, "MarketTime=\"", "\"")[:-4]
-					open_ = float(find_between(r.text, "OpenPrice=\"", "\""))
-					high = float(find_between(r.text, "HighPrice=\"", "\""))
-					low = float(find_between(r.text, "LowPrice=\"", "\""))
-					vol = int(find_between(r.text, "Volume=\"", "\""))
-					prev_close = float(find_between(r.text, "ClosePrice=\"", "\""))
-
-					price = float(find_between(r.text, "LastPrice=\"", "\""))
-
-					if price<1:
-						price = round(price,3)
-					else:
-						price = round(price,2)
+			# if price<1:
+			# 	price = round(price,3)
+			# else:
+			# 	price = round(price,2)
 
 
-					#ts = timestamp(time[:5])
+			#ts = timestamp(time[:5])
 
-					pipe.put(["Connected",symbol,time,price,open_,high,low,vol,prev_close])
+			pipe.put(["Connected",symbol,time,price,open_,high,low,vol,prev_close])
 
-					p="http://localhost:8080/Deregister?symbol="+symbol+"&feedtype=L1"
-					r= requests.get(p)
+			# p="http://localhost:8080/Deregister?symbol="+symbol+"&feedtype=L1"
+			# r= requests.get(p)
 
-				#pipe.put(output)
+			#print(symbol+" complete")
 
-			except Exception as e:
-				print("Get info error:",e)
-				connection_error = True
-				pipe.put(["Ppro Error",symbol])
-				lock[symbol] = False
+		except Exception as e:
+			print("Get info error:",e)
+			#connection_error = True
+			pipe.put(["Not good",symbol])
+			lock[symbol] = False
+
+	print(symbol+" finish")
 
 
-global connection_error
-connection_error = False
+# global connection_error
+# connection_error = False
 global lock
 lock = {}
 
@@ -173,14 +170,17 @@ ms.start()
 count = 0
 while True:
     data = queue.get()
-    print(data)
+    #print(data)
 
     count +=1 
 
+    with sync_lock:
+    	sync_number -=1
+
     if count%10 == 0:
     	print("Simaltaneous process:",sync_number)
-    	with sync_lock:
-    		sync_number -=10
+    	print("received:",count)
+
 
 
 
