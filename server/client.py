d058b1277a5c6620ee52299cd40a56d516f0ef3e
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-01-27 14:18:06 -0500

    Update client.py

--- server/client.py
+++ server/client.py
@@ -4,64 +4,55 @@ import pickle
 import pandas as pd
 import time
 
-HOST = '10.29.10.132'  # The server's hostname or IP address
-PORT = 65421       # The port used by the server
 
 print(socket.gethostname())
+HOST = '10.29.10.132'  # The server's hostname or IP address
+PORT = 65421       # The port used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 
-connected = False
+while True:
 
-while not connected:
-	try:
-		s.connect((HOST, PORT))
-		connected = True
-	except:
-		print("Cannot connected. Try again in 2 seconds.")
-		time.sleep(2)
+	print("Trying to connect to the server")
+	connected = False
 
-
-
-connection = True
-print("Connection Successful")
-while connection:
-	try:
-		s.sendall(b'Alive check')
-	except:
-		connection = False
-		break
-	data = []
-	while True:
+	while not connected:
 		try:
-			part = s.recv(2048)
+			s.connect((HOST, PORT))
+			connected = True
+		except:
+			print("Cannot connected. Try again in 2 seconds.")
+			time.sleep(2)
+
+
+
+	connection = True
+	print("Connection Successful")
+	while connection:
+		try:
+			s.sendall(b'Alive check')
 		except:
 			connection = False
 			break
-		#if not part: break
-		print(len(part))
-		data.append(part)
-		if len(part) < 2048:
-			#try to assemble it, if successful.jump. else, get more. 
+		data = []
+		while True:
 			try:
-				k = pickle.loads(b"".join(data))
-				break
+				part = s.recv(2048)
 			except:
-				pass
-	print(k)
+				connection = False
+				break
+			#if not part: break
+			print(len(part))
+			data.append(part)
+			if len(part) < 2048:
+				#try to assemble it, if successful.jump. else, get more. 
+				try:
+					k = pickle.loads(b"".join(data))
+					break
+				except:
+					pass
+		print(k)
 
-print("Server disconnected")
+	print("Server disconnected")
 
-
-
-def recvall(sock):
-	BUFF_SIZE = 1024 # 4 KiB
-	data = b''
-	while True:
-		part = sock.recv(BUFF_SIZE)
-		data += part
-		if len(part) < BUFF_SIZE:
-			# either 0 or end of data
-			break
-	return data
+	#restarted the whole thing 
