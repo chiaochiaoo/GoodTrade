2efb58d0276b3cc13ff345376126b787a96ebd2c
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-01-27 13:59:35 -0500

    Update client.py

--- server/client.py
+++ server/client.py
@@ -2,6 +2,7 @@
 import socket
 import pickle
 import pandas as pd
+import time
 
 HOST = '10.29.10.132'  # The server's hostname or IP address
 PORT = 65421       # The port used by the server
@@ -9,7 +10,19 @@ PORT = 65421       # The port used by the server
 print(socket.gethostname())
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-s.connect((HOST, PORT))
+
+
+connected = False
+
+while not connected:
+	try:
+		s.connect((HOST, PORT))
+		connected = True
+	except:
+		print("Cannot connected. Try again in 2 seconds.")
+		time.sleep(2)
+
+
 
 connection = True
 print("Connection Successful")
@@ -22,20 +35,20 @@ while connection:
 	data = []
 	while True:
 		try:
-			part = s.recv(256)
+			part = s.recv(2048)
 		except:
 			connection = False
 			break
 		#if not part: break
 		print(len(part))
 		data.append(part)
-		if len(part) < 256:
-			break
-
-	try:
-		k = pickle.loads(b"".join(data))
-	except:
-		pass
+		if len(part) < 2048:
+			#try to assemble it, if successful.jump. else, get more. 
+			try:
+				k = pickle.loads(b"".join(data))
+				break
+			except:
+				pass
 	print(k)
 
 print("Server disconnected")
