9fc1c4d67c34c43376bec760ef0ea1e86fdd45f4
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2021-02-08 21:46:06 -0300

    Update scanner_process_manager.py

--- scanner_process_manager.py
+++ scanner_process_manager.py
@@ -59,10 +59,12 @@ class scanner_process_manager:
 
 	def receive_request(self):
 
+		count = 0
 		while True:
 			print("manager reciving....:")
 			d = self.request.recv()
-			print("manager info received",d)
+			count +=1
+			print("manager info received",count,d[:1])
 
 		#print(d)
 		#check if it is normal type?
