21022e0bcb2faf7b15c22d6107c2c14f27601e0d
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-05-19 09:07:38 -0400

    Update Manager.py

--- cores/algo_manager v2/Manager.py
+++ cores/algo_manager v2/Manager.py
@@ -599,7 +599,7 @@ if __name__ == '__main__':
 	
 
 	root = tk.Tk() 
-	root.title("GoodTrade Algo Manager v2 b1") 
+	root.title("GoodTrade Algo Manager v2 b2") 
 	root.geometry("1920x800")
 
 	Manager(root,goodtrade_pipe,ppro_out,ppro_in,TEST)
