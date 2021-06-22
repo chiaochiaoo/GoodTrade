f63438b5a10a17881248680309ca22fd234e25d3
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-06-22 15:49:12 -0400

    Update Ppro_in.py

--- cores/algo_manager v2/Ppro_in.py
+++ cores/algo_manager v2/Ppro_in.py
@@ -4,9 +4,12 @@ import socket
 import threading
 from constant import *
 from Util_functions import *
-
+import csv
 
 def Ppro_in(port,pipe):
+
+
+	l1data = {}
 
 	UDP_IP = "localhost"
 	UDP_PORT = port
@@ -20,6 +23,18 @@ def Ppro_in(port,pipe):
 	log_print("Socket Created:",sock)
 	
 	work=False
+
+
+	try:
+		f = open("../../algo_logs/"+datetime.now().strftime("%m-%d")+"data.csv", "x")
+	except:
+		f = open("../../algo_logs/"+datetime.now().strftime("%m-%d")+"data.csv", "w")
+
+	writer = csv.writer(f,lineterminator = '\n')
+	writer.writerow(['symbol', 'timestamp','bid','ask'])
+	
+
+
 	pipe.send(["msg","algo_ppro working"])
 	while True:
 		data, addr = sock.recvfrom(1024)
@@ -32,8 +47,9 @@ def Ppro_in(port,pipe):
 		if type_ == "OrderStatus":
 			decode_order(stream_data,pipe)
 		elif type_ =="L1":
-			decode_l1(stream_data,pipe)
+			decode_l1(stream_data,pipe,writer,l1data)
 
+	f.close()
 def ppro_connection_service(pipe,port):
 
 	#keep running and don't stop
@@ -86,6 +102,21 @@ def timestamp_seconds(s):
 	except Exception as e:
 		log_print("Timestamp conversion error:",e,s)
 		return 0
+
+def timestamp_mili_seconds(s):
+
+	#s="15:41:56.526"
+
+	p = s[:-4].split(":")
+	mili = int(s[-3:])
+	try:
+		x = int(p[0])*3600+int(p[1])*60+int(p[2])
+		x2 = x*1000 + mili
+		return x,x2
+	except Exception as e:
+		log_print("Timestamp conversion error:",e,s)
+		print("")
+
 def find_between(data, first, last):
 	try:
 		start = data.index(first) + len(first)
@@ -148,19 +179,60 @@ def decode_order(stream_data,pipe):
 			pipe.send(["order rejected",data])
 
 
-def decode_l1(stream_data,pipe):
+def decode_l1(stream_data,pipe,writer,l1data):
 	symbol = find_between(stream_data, "Symbol=", ",")
-	bid=find_between(stream_data, "BidPrice=", ",")
-	ask=find_between(stream_data, "AskPrice=", ",")
-	ts=find_between(stream_data, "MarketTime=", ",")[:-4]
+	bid=float(find_between(stream_data, "BidPrice=", ","))
+	ask=float(find_between(stream_data, "AskPrice=", ","))
+	#ts= timestamp_seconds(find_between(stream_data, "MarketTime=", ",")[:-4])
 
-	data ={}
-	data["symbol"]= symbol
-	data["bid"]= float(bid)
-	data["ask"]= float(ask)
-	data["timestamp"]= timestamp_seconds(ts)
-	#add time
-	pipe.send(["order update",data])
+	ts,mili_ts = timestamp_mili_seconds(find_between(stream_data, "MarketTime=", ","))
+
+	send = False
+
+	if symbol in l1data:
+		#if either level has changed. register. 
+		if l1data[symbol]["bid"]!=bid or l1data[symbol]["ask"]!=ask:
+			send = True
+		elif ts-l1data[symbol]["timestamp"] >1:
+			send = True
+		#if has been more then 2 leconds. registered.
+
+	else:
+		l1data[symbol] = {}
+		l1data[symbol]["symbol"] = symbol
+		l1data[symbol]["bid"] = bid
+		l1data[symbol]["ask"] = ask
+		l1data[symbol]["timestamp"] = ts
+		send = True
+
+	if send:
+		# data ={}
+		# data["symbol"]= symbol
+		# data["bid"]= bid
+		# data["ask"]= ask
+		# data["timestamp"]= ts
+
+		l1data[symbol]["symbol"] = symbol
+		l1data[symbol]["bid"] = bid
+		l1data[symbol]["ask"] = ask
+		l1data[symbol]["timestamp"] = ts
+		#print(l1data[symbol])
+		#add time
+		pipe.send(["order update",l1data[symbol]])
+		writer.writerow([symbol,mili_ts,bid,ask])
 
 # x= "LocalTime=15:56:54.742,Message=OrderStatus,MarketDateTime=20210504-15:56:55.000,Currency=1,Symbol=QQQ.NQ,Gateway=2030,Side=T,OrderNumber=QIAOSUN_02000326M1791F8100000,Price=329.540000,Shares=70,Position=4,OrderState=Filled,CurrencyChargeGway=1,ChargeGway=0.21000,CurrencyChargeAct=1,ChargeAct=0.0096600,CurrencyChargeSec=1,ChargeSec=0.47750,CurrencyChargeExec=0,ChargeExec=0,CurrencyChargeClr=1,ChargeClr=0.012950,OrderFlags=129,CurrencyCharge=1,Account=1TRUENV001TNVQIAOSUN_USD1,InfoCode=255,InfoText=LiqFlags:^Tag30:14^Tag31:329.5400000^Tag150:2^Tag9730:"
 # log_print(find_between(x, "MarketDateTime=", ",")[9:-4])
+
+
+# try:
+# 	f = open("../../algo_logs/"+datetime.now().strftime("%m-%d")+"data.csv", "x")
+# except:
+# 	f = open("../../algo_logs/"+datetime.now().strftime("%m-%d")+"data.csv", "w")
+
+
+# writer = csv.writer(f,lineterminator = '\n')
+# writer.writerow(['symbol', 'timestamp','bid','ask'])
+
+# for i in range(10):
+# 	writer.writerow(['1', '2','3',i])
