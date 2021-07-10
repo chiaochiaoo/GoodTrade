2a7903d724f173538abf4c0ee664355cc6ddd20d
chiaochiaoo <zenvoidsun@gmail.com>
2021-07-10 20:05:58 -0300

    Update TNV_Scanner.py

--- modules/TNV_Scanner.py
+++ modules/TNV_Scanner.py
@@ -40,12 +40,16 @@ class TNV_Scanner():
 		self.or_frame = tk.Canvas(self.TNV_TAB)
 		self.ob_frame = tk.Canvas(self.TNV_TAB)
 		self.pb_frame = tk.Canvas(self.TNV_TAB)
+		self.cp_frame = tk.Canvas(self.TNV_TAB)
+
 
 		self.TNV_TAB.add(self.or_frame, text ='Open Reversal')
 		self.TNV_TAB.add(self.vb_frame, text ='Volatility Break')
 		
 		self.TNV_TAB.add(self.ob_frame, text ='Open Break')
 		self.TNV_TAB.add(self.pb_frame, text ='Premarket Pick')
+
+		self.TNV_TAB.add(self.cp_frame, text ='Corey\'s Pick')
 
 		# self.breakout_frame = ttk.LabelFrame(self.root,text="Volatility Breakout")
 		# self.breakout_frame.place(x=0, rely=0.05, relheight=1, relwidth=0.95)
@@ -69,6 +73,7 @@ class TNV_Scanner():
 		self.volatility_scanner = Volatility_break(self.vb_frame,NT)
 		self.open_reversal = Open_Reversal(self.or_frame,NT)
 		self.open_break = Open_Break(self.ob_frame,NT)
+		self.corey_pick = CoreysPick(self.cp_frame,NT)
 		#self.update_entry()
 
 	def update_entry(self,data):
@@ -420,6 +425,117 @@ class Open_Break():
 		except Exception as e:
 			print("TNV scanner construction open break:",e)
 
+class CoreysPick():
+	def __init__(self,root,NT):
+		self.buttons = []
+		self.entries = []
+		self.l = 1
+		self.NT = NT
+		self.labels_width = [9,6,5,7,5,5,6,6,6,6,8,6]
+		self.labels = ["Symbol","A.Vol","Rel.V","Br.SCORE","5M","SO%","SC%","Listed","Since","Last","Ignore","Add"]
+		self.file = "signals/Coreypick"+datetime.now().strftime("%m-%d")+".csv"
+		self.root = root
+		self.recreate_labels(self.root)
+
+	def recreate_labels(self,frame):
+
+		self.labels_position = {}
+		self.labels_position["Rank"]=0
+		self.labels_position["Symbol"]=1
+		self.labels_position["Market"]=2
+		self.labels_position["Price"]=3
+		self.labels_position["Since"]=4
+		self.labels_position["Been"]=5
+		self.labels_position["SC%"]=6
+		self.labels_position["SO%"]=7
+		self.labels_position["L5R%"]=8
+		self.labels_position["Status"]=9
+		self.labels_position["Add"]=10
+
+		self.market_sort = [0,1,2]#{'NQ':0,'NY':1,'AM':2}
+
+		self.status_code = {}
+		self.status_num = 0
+
+		for i in range(len(self.labels)): #Rows
+			self.b = tk.Button(self.root, text=self.labels[i],width=self.labels_width[i])#,command=self.rank
+			self.b.configure(activebackground="#f9f9f9")
+			self.b.configure(activeforeground="black")
+			self.b.configure(background="#d9d9d9")
+			self.b.configure(disabledforeground="#a3a3a3")
+			self.b.configure(relief="ridge")
+			self.b.configure(foreground="#000000")
+			self.b.configure(highlightbackground="#d9d9d9")
+			self.b.configure(highlightcolor="black")
+			self.b.grid(row=self.l, column=i)
+			self.buttons.append(self.b)
+
+		self.l+=1
+		self.create_entry()
+
+	def create_entry(self):
+
+		for k in range(0,50):
+
+			self.entries.append([])
+
+			for i in range(len(self.labels)): #Rows
+				self.b = tk.Label(self.root, text=" ",width=self.labels_width[i])#,command=self.rank
+				self.b.grid(row=self.l, column=i)
+				self.entries[k].append(self.b)
+			self.l+=1
+
+
+
+	def update_entry(self,data):
+
+		#at most 8.
+
+		#["Symbol","A.Vol","Rel.V","Br.SCORE","5M","SO%","SC%","Listed","Since","Last","Ignore","Add"]
+		df = data
+
+
+		df.to_csv("open_break_out.csv")
+		entry = 0
+
+		try:
+			for index, row in df.iterrows():
+				#print(row)
+				rank = index
+				vol = row['Avg VolumeSTR']
+				relv = row['rel vol']
+				brscore = row['score2']
+				roc5 = row['5ROCP']
+				so = row['SO']
+				sc = row['SC']
+				since = row['since']
+				last = row['last']
+
+				############ add since, and been to the thing #############
+				if rank in self.NT.nasdaq_trader_symbols_ranking:
+					listed = str(self.NT.nasdaq_trader_symbols_ranking[rank])
+				else:
+					listed = "No"
+				#print(self.NT.nasdaq_trader_symbols)
+				if 1: #score>0:	
+
+					lst = [rank,vol,relv,brscore,roc5,so,sc,listed,since,last]
+
+					for i in range(len(lst)):
+						self.entries[entry][i]["text"] = lst[i]
+					entry+=1
+					if entry ==50:
+						break
+
+			while entry<50:
+				#print("ok")
+				for i in range(10):
+					self.entries[entry][i]["text"] = ""
+				entry+=1
+
+		except Exception as e:
+			print("TNV scanner construction open break:",e)
+
 if __name__ == '__main__':
 
 	root = tk.Tk() 
@@ -432,7 +548,7 @@ if __name__ == '__main__':
 
 
 
-			
+
 	# 		info = [rank,avgv,relv,roc5,roc10,roc15,score,sc,so]
 	# 		self.nasdaq.append([])
 
