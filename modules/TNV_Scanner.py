1c0a7cf6c4f660b0e6fc02afd9dfe5424fa0e387
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-07-30 09:41:23 -0400

    Update TNV_Scanner.py

--- modules/TNV_Scanner.py
+++ modules/TNV_Scanner.py
@@ -383,6 +383,8 @@ class Open_Reversal():
 
 								self.entries[entry][8]["command"]= lambda symbol=rank,support=support,resistence=resistence,risk_=self.algo_risk:self.send_algo(symbol,support,resistence,risk_)
 
+								if self.algo_activate.get()==1:
+									self.send_algo(rank,support,resistence,self.algo_risk)
 							if i == ts_location:
 								self.entries[entry][i]["text"] = ts_to_min(lst[i])
 							else:
