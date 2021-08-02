a166328a7f2749d6b537d554ab57c0198aa9b274
chiaochiaoo <zenvoidsun@gmail.com>
2021-08-02 16:23:06 -0300

    Update TOS_visualizer.py

--- modules/TOS_visualizer.py
+++ modules/TOS_visualizer.py
@@ -14,6 +14,7 @@ import csv
 import requests
 import threading
 import socket
+
 def find_between(data, first, last):
     try:
         start = data.index(first) + len(first)
@@ -30,7 +31,6 @@ class moudule_2:
 	def __init__(self,  window,symbol):
 		self.window = window
 		#self.box = Entry(window)
-		
 		# self.button = Button (window, text="check", command=self.plot)
 		# self.box.pack ()
 		# self.button.pack()
@@ -38,21 +38,36 @@ class moudule_2:
 		# self.button2.pack()
 		self.i = 0
 
+		self.update_complete = IntVar()
+
+		self.vol1 = IntVar()
+		self.trade1 = IntVar()
+
+		self.vol5 = IntVar()
+		self.trade5 = IntVar()
+
+		self.vol60 = IntVar()
+		self.trade60 = IntVar()
+
 		self.c1={"tms":0,"v":0,"t":0,"ts":[],"vs":[]}
 		self.c5={"tms":0,"v":0,"t":0,"ts":[],"vs":[]}
 		self.c60={"tms":0,"v":0,"t":0,"ts":[],"vs":[]}
+
 		self.plot()
-		self.register(symbol)
-		#dc = threading.Thread(target=self.TOS_listener, daemon=True)
+		#self.register(symbol)
+
+		dc = threading.Thread(target=self.TOS_listener, daemon=True)
 		#dc.start()
 
-		up = threading.Thread(target=self.update_graph, daemon=True)
-		up.start()
+		#up = threading.Thread(target=self.update_graph, daemon=True)
+		#up.start()
 
 		#self.setx()
 
 		#self.TOS_listener()
 		#self.update_graph()
+
+
 
 	def setx(self,count):
 		#self.v.append(self.v[-1]+1)
@@ -71,7 +86,8 @@ class moudule_2:
 
 		print("socket start")
 
-		
+		trades= [self.trade1,self.trade5,self.trade60]
+		volume = [self.vol1,self.vol5,self.vol60]
 		d = [self.c1,self.c5,self.c60]
 
 		while True:
@@ -82,7 +98,6 @@ class moudule_2:
 			t1 = get_sec(time[:-4])
 			size = int(find_between(stream_data, "Size=", ","))
 			price = float(find_between(stream_data, "Price=", ","))
-			
 
 			t5 = t1//5
 			t60= t1//60
@@ -99,15 +114,19 @@ class moudule_2:
 					obj["ts"].append(obj["t"])
 					obj["vs"].append(obj["v"])
 
+					volume[i].set(obj["v"])
+					trades[i].set(obj["t"])
+
 					increment = True
 					obj["v"]=size
 					obj["t"]=1
 				else:
 					obj["v"]+=size
-					obj["t"]+=1			
+					obj["t"]+=1
 					print(self.c1)
 			if increment: ###bar, every second. Distribution, every 5 second. (with cap capacity.)
 				#print("update")
+				self.update_complete.set(self.update_complete.get()+1)
 				print(self.c1)
 				#for i in range(3):
 					# self.vol[self.timeframe[i]].cla()
@@ -123,22 +142,6 @@ class moudule_2:
 				# 	self.trades[self.timeframe[i]+"current"].set_data(1.15,[0,1])
 
 				# self.fig.canvas.draw()
-
-
-	def update_graph(self):
-
-		count = 0
-		while True:
-			self.setx(count)
-
-			count+=1
-			# for i in range(3):
-			# 	self.vol[self.timeframe[i]+"current"].set_data(count,[0,1])
-			# 	self.trades[self.timeframe[i]+"current"].set_data(count,[0,1])
-
-			# self.fig.canvas.draw()
-			# count+=1
-			time.sleep(1)
 
 	def simulated_input(self):
 		c1={"tms":0,"v":0,"t":0,"ts":[],"vs":[]}
@@ -196,11 +199,24 @@ class moudule_2:
 
 		print("done")
 
+
+	def update_curret(self):
+		#Call every second.
+
+		lst = [self.vol1,self.vol5,self.vol60,self.trade1,self.trade5,self.trade60]
+		for i in range(6):
+			self.vol[self.timeframe[i%3]+"current"].set_data(lst[i].get(),[0,1])
+			self.trades[self.timeframe[i%3]+"current"].set_data(lst[i].get(),[0,1])
+
+		self.figc.canvas.draw()
+
+	def update_chart(self):
+		chart.set_data(val.get(),[0,1])
+		self.figc.canvas.draw()
+
 	def plot (self):
 
-
 		self.figc, axs = plt.subplots(2,3,figsize=(15,7))
-		#title = ["1s Volume","5s Volume","1m Volume","1s Trades","5s Trades","1m Trades"]
 		self.timeframe = ["1s","5s","1m"]
 		self.types = ["Volume","Trades"]
 
@@ -208,7 +224,6 @@ class moudule_2:
 		self.trades = {}
 
 		total =[self.vol,self.trades]
-
 		outlier = dict(linewidth=3, color='darkgoldenrod',marker='o')
 
 		self.charts={}
@@ -230,6 +245,9 @@ class moudule_2:
 
 			#self.charts[title[i]] = axs[i//3][i%3]
 		#print(self.vol)
+
+		self.update_complete.trace('w',self.update_chart)
+
 		self.canvas = FigureCanvasTkAgg(self.figc, master=self.window)
 		self.canvas.get_tk_widget().pack()
 		self.canvas.draw()
