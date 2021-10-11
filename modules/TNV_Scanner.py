e48e8fc4fc813f16b3eb4371199e03eef4280581
chiaochiaooServer <zenvoidsun@gmail.com>
2021-10-11 10:32:39 -0400

    Update TNV_Scanner.py

--- modules/TNV_Scanner.py
+++ modules/TNV_Scanner.py
@@ -616,14 +616,18 @@ class Open_Reversal():
 		self.l+=1
 		self.create_entry()
 
-	def send_algo(self,symbol,support,resistence):
+	def send_algo(self,symbol,support,resistence,side):
 
 		#self.entries[entry][8]["command"]= lambda symbol=rank,side=side,open_=row['open'],stop_=rscore,risk_=self.algo_risk:self.send_algo(self,symbol,side,open_,stop_,risk_)
 		risk = self.algo_risk.get()
 
 		if risk>0:
-			info = ["New order",["BreakAny",symbol,support,resistence,risk,{},"deploy","Fib only"]]
-			#print("sending",info)
+
+			if side =="UP":
+				info = ["New order",["BreakAny",symbol,support-0.02,resistence,risk,{},"deploy","Fib only"]]
+				#print("sending",info)
+			else:
+				info = ["New order",["BreakAny",symbol,support,resistence+0.02,risk,{},"deploy","Fib only"]]
 			self.tnv_scanner.send_algo(info)
 
 	def send_group_algos(self,lst):
@@ -656,38 +660,41 @@ class Open_Reversal():
 		self.hour = tk.IntVar(value=9)
 		self.minute = tk.IntVar(value=30)
 
-		ttk.Label(frame, text="Hour:").grid(sticky="w",column=col,row=row)
+		ttk.Label(frame, text="Start:").grid(sticky="w",column=col,row=row)
 		ttk.Entry(frame, textvariable=self.hour).grid(sticky="w",column=col+1,row=row)
 
-		ttk.Label(frame, text="Minute").grid(sticky="w",column=col+2,row=row)
+		ttk.Label(frame, text=":").grid(sticky="w",column=col+2,row=row)
 		ttk.Entry(frame, textvariable=self.minute).grid(sticky="w",column=col+3,row=row)
 
 
 		self.rel_v = tk.DoubleVar(value=0)
 		self.re_score = tk.DoubleVar(value=0)
 
+
+		self.ehour = tk.IntVar(value=10)
+		self.eminute = tk.IntVar(value=00)
 		row = 3
 		col = 1
-		ttk.Label(frame, text="Rel V>=").grid(sticky="w",column=col,row=row)
-		ttk.Entry(frame, textvariable=self.rel_v).grid(sticky="w",column=col+1,row=row)
+		ttk.Label(frame, text="End").grid(sticky="w",column=col,row=row)
+		ttk.Entry(frame, textvariable=self.ehour).grid(sticky="w",column=col+1,row=row)
 
-		ttk.Label(frame, text="Score>").grid(sticky="w",column=col+2,row=row)
-		ttk.Entry(frame, textvariable=self.re_score).grid(sticky="w",column=col+3,row=row)
+		ttk.Label(frame, text=":").grid(sticky="w",column=col+2,row=row)
+		ttk.Entry(frame, textvariable=self.eminute).grid(sticky="w",column=col+3,row=row)
 
 		self.side_ = tk.StringVar(value='x')
 		self.listed_ = tk.BooleanVar(value=False)
 		
-		row = 4
-		col = 1
+		# row = 4
+		# col = 1
 
 
-		ttk.Label(frame, text="Side:").grid(sticky="w",column=col,row=row)
-		l={"Up","Down","Any","Any"}
-		ttk.OptionMenu(frame, self.side_, *sorted(l)).grid(sticky="w",column=col+1,row=row)
+		# ttk.Label(frame, text="Side:").grid(sticky="w",column=col,row=row)
+		# l={"Up","Down","Any","Any"}
+		# ttk.OptionMenu(frame, self.side_, *sorted(l)).grid(sticky="w",column=col+1,row=row)
 
 
-		ttk.Label(frame, text="Listed?").grid(sticky="w",column=col+2,row=row)
-		ttk.Checkbutton(frame, variable=self.listed_).grid(sticky="w",column=col+3,row=row)
+		# ttk.Label(frame, text="Listed?").grid(sticky="w",column=col+2,row=row)
+		# ttk.Checkbutton(frame, variable=self.listed_).grid(sticky="w",column=col+3,row=row)
 
 
 		algo_timer = self.hour.get()*60 + self.minute.get()
@@ -722,6 +729,8 @@ class Open_Reversal():
 		ts = now.hour*60+now.minute
 		
 		algo_timer = self.hour.get()*60 + self.minute.get()
+
+		end_timer = self.ehour.get()*60 + self.eminute.get()
 
 		df = data
 
@@ -766,7 +775,7 @@ class Open_Reversal():
 
 						for i in range(len(lst)):
 							
-							if lst[ts_location] >=ts and lst[ts_location]>=algo_timer:
+							if lst[ts_location] >=ts and lst[ts_location]>=algo_timer and lst[ts_location]<=end_timer:
 								self.entries[entry][i]["background"] = "LIGHTGREEN"
 								self.entries[entry][8].grid()
 
@@ -777,7 +786,7 @@ class Open_Reversal():
 									support = row['open']
 									resistence = row['high']
 
-								self.entries[entry][8]["command"]= lambda symbol=rank,support=support,resistence=resistence:self.send_algo(symbol,support,resistence)
+								self.entries[entry][8]["command"]= lambda symbol=rank,support=support,side=side,resistence=resistence:self.send_algo(symbol,support,resistence,side)
 
 								if self.algo_activate.get()==1:
 									if rank not in self.algo_placed:
@@ -790,6 +799,9 @@ class Open_Reversal():
 										order["symbol"] = rank
 										order["support"] = support
 										order["resistence"] = resistence
+
+										order["side"] = side
+
 										send_algo.append(order)
 
 										#print(rank,self.algo_placed)
@@ -1224,7 +1236,6 @@ class CoreysPick():
 				self.b.grid(row=self.l, column=i)
 				self.entries[k].append(self.b)
 			self.l+=1
-
 
 
 	def update_entry(self,data):
