b212a3ed01316f93b905beffa69b5b492b384c46
chiaochiaooServer <zenvoidsun@gmail.com>
2021-10-27 09:55:01 -0400

    Update viewer.py

--- viewer.py
+++ viewer.py
@@ -447,7 +447,7 @@ if __name__ == '__main__':
 
 
 	root = tk.Tk() 
-	root.title("GoodTrade v508") 
+	root.title("GoodTrade v510") 
 	root.geometry("1800x900")
 	root.minsize(1500, 600)
 	root.maxsize(3000, 1500)
