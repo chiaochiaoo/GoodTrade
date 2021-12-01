5a4f1bcef5b80257085d08098b8ca75a1427c154
chiaochiaoo <chiaochiao@users.noreply.github.com>
2021-12-01 07:43:10 -0500

    511

--- viewer.py
+++ viewer.py
@@ -452,7 +452,7 @@ if __name__ == '__main__':
 
 
 	root = tk.Tk() 
-	root.title("GoodTrade v510") 
+	root.title("GoodTrade v511") 
 	root.geometry("1800x900")
 	root.minsize(1500, 600)
 	root.maxsize(3000, 1500)
