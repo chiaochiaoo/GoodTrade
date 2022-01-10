2e2e9312d12441a5e3d53f6e7cf626cbba8704a5
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2022-01-10 09:39:10 -0300

    Update TNV_Scanner.py

--- modules/TNV_Scanner.py
+++ modules/TNV_Scanner.py
@@ -161,6 +161,7 @@ class TNV_Scanner():
 			#trim pb1 , pb2.
 			pb = pd.concat([pb1,pb2])
 			pb = pb.reindex(pb.SC.abs().sort_values(ascending=False).index)[:30]
+			#print(pb)
 
 
 		else:
@@ -170,7 +171,7 @@ class TNV_Scanner():
 		# pb1 = pb1.sort_values(by=["SC"],ascending=False)[:15]
 		# pb2 = pb2.sort_values(by=["SC"],ascending=True)[:15]
 
-		pb3 = filtered_df.loc[((filtered_df["SC"]<=5)|(filtered_df["SC"]>=5))&(filtered_df["Market Cap"]==5)][:10]
+		pb3 = filtered_df.loc[((filtered_df["SC"]<=-5)|(filtered_df["SC"]>=5))&(filtered_df["Market Cap"]==5)][:10]
 		pb = pd.concat([pb,pb3])
 
 		pb = pb.reindex(pb.SC.abs().sort_values(ascending=False).index)[:35]
