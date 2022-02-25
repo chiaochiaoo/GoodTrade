a1fa4d6e8d5c32681d777540a3c7f05a6d9973aa
chiaochiaooServer <zenvoidsun@gmail.com>
2022-02-25 13:03:42 -0500

    Update viewer.py

--- viewer.py
+++ viewer.py
@@ -452,7 +452,7 @@ if __name__ == '__main__':
 
 
 	root = tk.Tk() 
-	root.title("GoodTrade v526") 
+	root.title("GoodTrade v527 - Scanner Stable Fix") 
 	root.geometry("1800x900")
 	root.minsize(1500, 600)
 	root.maxsize(3000, 1500)
