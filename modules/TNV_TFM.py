3cc4ef26d1e2db82ac082c8c428b01b92ef8450c
chiaochiaoo <chiaochiao@users.noreply.github.com>
2022-04-08 12:53:51 -0400

    Update TNV_TFM.py

--- modules/TNV_TFM.py
+++ modules/TNV_TFM.py
@@ -626,7 +626,7 @@ class SinlgeTrade():
 				vals = ["S",new_order["algo_id"],new_order["algo_name"],new_order["symbol"],new_order["entry_type"],new_order["support"],new_order["resistence"],new_order["risk"],new_order["side"],"T",new_order["management"]]
 
 
-				msg = "localhost:4441/"
+				msg = "http://localhost:4441/"	
 
 				for i in range(len(strs)):
 
