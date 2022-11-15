242f7ae69b0fd2ba7b1486a2c5dd690dce8189a5
chiaochiaoo <zenvoidsun@gmail.com>
2022-11-15 09:54:46 -0500

    Update Manager.py

--- cores/algo_manager v5/Manager.py
+++ cores/algo_manager v5/Manager.py
@@ -865,10 +865,10 @@ class Manager:
 					self.record["detes"][ALGO][idx] =real
 
 				if "TOTAL" not in self.record["detes"]:
-					self.record["detes"][ALGO] = [0 for i in range(390)]
-					self.record["detes"][ALGO][idx] =self.record['total']['net']
+					self.record["detes"]["TOTAL"] = [0 for i in range(390)]
+					self.record["detes"]["TOTAL"][idx] =self.record['total']['net']
 				else:
-					self.record["detes"][ALGO][idx] =self.record['total']['net']
+					self.record["detes"]["TOTAL"][idx] =self.record['total']['net']
 			with open(self.file, 'w') as f:
 				json.dump(self.record, f)
 			#print(self.record)
