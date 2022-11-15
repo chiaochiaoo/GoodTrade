9f38a140ba2f7282c20115f50352a2c6745f8a58
chiaochiaoo <zenvoidsun@gmail.com>
2022-11-15 11:47:42 -0500

    Update converter.py

--- cores/algo_manager v5/converter.py
+++ cores/algo_manager v5/converter.py
@@ -1,7 +1,6 @@
 import json
 import os
 import csv
-
 dates = {}
 with open('2022_10.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -17,8 +16,9 @@ with open('2022_10.csv', newline='') as csvfile:
             
             if d not in dates:
                 dates[d] = {}
+                dates[d]["algos"] = {}
             else:
-                dates[d][a] = r
+                dates[d]["algos"][a] = r
         c+=1
 with open('2022_11.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -34,8 +34,9 @@ with open('2022_11.csv', newline='') as csvfile:
             
             if d not in dates:
                 dates[d] = {}
+                dates[d]["algos"] = {}
             else:
-                dates[d][a] = r
+                dates[d]["algos"][a] = r
         c+=1
         
 for i in dates.keys():
