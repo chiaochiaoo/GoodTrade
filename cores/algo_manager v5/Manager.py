7a0d65c578f3666090796900590cee7bacabb403
chiaochiaoo <chiaochiao@users.noreply.github.com>
2023-03-13 12:12:38 -0400

    go

--- cores/algo_manager v5/Manager.py
+++ cores/algo_manager v5/Manager.py
@@ -1020,8 +1020,8 @@ class Manager:
 
 						if symbol in self.symbols_short:
 							self.symbol_data[self.symbols_short[symbol]].update_price(price,price,0)
-									
-					print("price update complete.. symbols:",len(symbols),x)
+							print(symbol,self.symbols_short[symbol],self.symbol_data[self.symbols_short[symbol]].get_bid())
+					print("Manager: price update complete.. symbols:",len(symbols),x)
 		except	Exception	as e:
 			PrintException("Updating prices error",e)
 
