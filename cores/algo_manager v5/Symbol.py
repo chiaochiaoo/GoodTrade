2ed8104b99893e21f67593dc6fb8755fead87c5f
chiaochiaooServer <zenvoidsun@gmail.com>
2023-07-19 15:48:54 -0400

    Update Symbol.py

--- cores/algo_manager v5/Symbol.py
+++ cores/algo_manager v5/Symbol.py
@@ -178,7 +178,7 @@ class Symbol:
 
 
 				if self.holding_update==False:
-					if self.difference!=0 and ts<=957:
+					if self.difference!=0 and ts<=958:
 						self.inspection_timestamp = timestamp
 						self.deploy_orders()
 						return 1
