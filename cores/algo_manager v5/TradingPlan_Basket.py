091054121fe5a7ae60a0bed131d9ac80ce10cb60
chiaochiaooServer <zenvoidsun@gmail.com>
2023-08-07 10:24:20 -0400

    flattening time limit

--- cores/algo_manager v5/TradingPlan_Basket.py
+++ cores/algo_manager v5/TradingPlan_Basket.py
@@ -161,7 +161,10 @@ class TradingPlan_Basket:
 
 		log_print(self.source,self.algo_name,"expect",symbol,shares," aggresive ", aggresive,"current have",self.current_shares[symbol])
 
-		if symbol not in self.banned and self.flatten_order!=True:
+		now = datetime.now()
+		ts = now.hour*3600 + now.minute*60 + now.second
+
+		if symbol not in self.banned and self.flatten_order!=True and ts<57600-60:
 			with self.read_lock[symbol]:
 
 				self.expected_shares[symbol] = shares
@@ -169,7 +172,7 @@ class TradingPlan_Basket:
 
 				if aggresive:
 					now = datetime.now()
-					ts = now.hour*3600 + now.minute*60 + now.second
+					ts = now.hour*60 + now.minute#*60 + now.second
 
 					if ts - self.recent_action_ts[symbol] >= 1 and ts<959*60:
 						self.recent_action_ts[symbol] = ts
