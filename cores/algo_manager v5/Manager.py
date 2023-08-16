9106001312e8f243a96fba115f1a8b97140b463d
chiaochiaoo <45312913+chiaochiaoo@users.noreply.github.com>
2023-08-16 15:48:30 -0400

    Update Manager.py

--- cores/algo_manager v5/Manager.py
+++ cores/algo_manager v5/Manager.py
@@ -571,7 +571,7 @@ class Manager:
 
 
 						for symbol,data in self.symbol_data.items():
-							total_moc[symbol] = data.get_all_future_remaining()
+							total_moc[symbol] = [symbol,data.get_all_future_remaining()]
 
 						log_print(total_moc)
 
